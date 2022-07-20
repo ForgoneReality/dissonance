bade2588c2aa2cc6720d7bb0b2e0f1decd5347ac
Cao Xu <46094706+ForgoneReality@users.noreply.github.com>
2022-07-20 16:03:37 -0700

    MORE HEROKU CHANGES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,12 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable" 
+  
+   config.action_cable.allowed_request_origins = [ 
+     'https://dissonance.herokuapp.com',  
+     'http://dissonance.herokuapp.com/']
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+mount ActionCable.server => '/cable'
+
 Rails.application.routes.draw do
   get 'api/Message'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
