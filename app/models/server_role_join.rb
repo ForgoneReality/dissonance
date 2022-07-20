101f925bc7c67242d52217dd1af61c184c8f7ffd
Cao Xu <46094706+ForgoneReality@users.noreply.github.com>
2022-07-20 16:24:27 -0700

    HEROKU PART 5 HOLY

--- app/models/server_role_join.rb
+++ app/models/server_role_join.rb
@@ -3,6 +3,7 @@ class ServerRoleJoin < ApplicationRecord
     validates :server_join_id, :role_id, presence: true
     validates :server_join_id, :uniqueness => { :scope => :role_id}
 
-    belongs_to: :server_join, class_name: "ServerJoin", foreign_key: :server_join_id
-    belongs_to: :role, class_name: "Role", foreign_key: :role_id
+    belongs_to :server_join, class_name: "ServerJoin", foreign_key: :server_join_id
+
+    belongs_to :role, class_name: "Role", foreign_key: :role_id
 end
