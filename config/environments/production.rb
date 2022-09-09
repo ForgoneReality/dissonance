e6916f881adb46fdf3a4bd487ca3c84d685c7f28
Cao Xu <46094706+ForgoneReality@users.noreply.github.com>
2022-09-08 22:37:58 -0700

    attmepting heroku fix #3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable" 
+  # config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable" 
   
-   config.action_cable.allowed_request_origins = [ 
-     'https://dissonance.herokuapp.com',  
-     'http://dissonance.herokuapp.com/']
+  #  config.action_cable.allowed_request_origins = [ 
+  #    'https://dissonance.herokuapp.com',  
+  #    'http://dissonance.herokuapp.com/']
 end
 
 
