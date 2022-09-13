f169879a25c3619c066cd8a3fca5f99769598bd8
Cao Xu <46094706+ForgoneReality@users.noreply.github.com>
2022-09-12 19:34:09 -0700

    Attempted to fix bug

--- frontend/components/modals/dropdown_modal.js
+++ frontend/components/modals/dropdown_modal.js
@@ -163,7 +163,8 @@ constructor(props) {
 
 const mapStateToProps = (state) => {
   let owner = false;
-  if(Object.keys(state.entities.channels).length > 0)
+  console.log("?");
+  if(state.ui.modal && Object.keys(state.entities.channels).length > 0)
   {
     const server_id = state.current.server.id
     owner = (state.session.id === state.entities.servers[server_id].owner_id);
